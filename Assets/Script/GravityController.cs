6ad9318b36552ff45d5a6fac90e024649802ec6b
SitiNurMuarivah <sitinurmuarivah92@gmail.com>
2023-05-22 03:35:12 +0700

    add CountDown

--- Assets/Script/GravityController.cs
+++ Assets/Script/GravityController.cs
@@ -5,22 +5,56 @@ using UnityEngine;
 public class GravityController : MonoBehaviour
 {
     [SerializeField] float acceleration = 9.8f;
+    Vector3 gravityOffset = Vector3.zero;
+    bool isActive = true;
 
     void Start()
     {
-        if(SystemInfo.supportsGyroscope)
-            Input.gyro.enabled = true;   
+        if (SystemInfo.supportsGyroscope)
+            Input.gyro.enabled = true;
     }
 
     void Update()
     {
+        if (isActive)
+        {
+            Physics.gravity = GetGravityFromSensor() + gravityOffset;
+        }
+        else
+        {
+            Physics.gravity = Vector3.zero;
+        }
+    }
+
+    public void CalibrateGravity()
+    {
+        gravityOffset = Vector3.down * acceleration - GetGravityFromSensor();
+        Debug.Log("sensor " + GetGravityFromSensor());
+        Debug.Log("offset " + gravityOffset);
+    }
+
+    public Vector3 GetGravityFromSensor()
+    {
         Vector3 gravity;
-        if(Input.gyro.gravity != Vector3.zero)
+        if (Input.gyro.gravity != Vector3.zero)
             gravity = Input.gyro.gravity * acceleration;
         else
             gravity = Input.acceleration * acceleration;
 
         gravity.z = Mathf.Clamp(gravity.z, float.MinValue, -1);
-        Physics.gravity = new Vector3(gravity.x, gravity.z, gravity.y);
+        return new Vector3(gravity.x, gravity.z, gravity.y);
+    }
+
+    public void SetActive(bool value)
+    {
+        isActive = value;
+        if (value)
+        {
+            Time.timeScale = 1;
+        }
+        else
+        {
+            Time.timeScale = 0;
+        }
     }
 }
